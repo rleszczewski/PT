cac6525a104026541d767f489888b3e3c31f7850
rleszczewski <rleszczewski@gmail.com>
2014-04-03 00:01:01 +0200

    Create zad5c.sh

---
+++ zad5c.sh
@@ -0,0 +1,12 @@
+if [[ $# < 1 ]]; then
+echo 'Za malo parametrow.'
+else
+for nazwa in `ls $1`;
+do
+n=`echo "$nazwa/" | tr '[:upper:]' '[:lower:]'`
+n=${n%/}
+if [[ $nazwa != $n ]]; then
+mv "$nazwa" "$n"
+fi
+done
+fi
